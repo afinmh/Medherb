ffc8cfd7ba8a3ed68b0f7eab1bcfa3707836ffaf
afinmh <afinmaulana522@gmail.com>
2025-08-24 15:50:59 +0700

    bismillah ga error

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Tambahkan fungsi async rewrites ini
-  async rewrites() {
-    return [
-      {
-        // Aturan ini akan mencocokkan SEMUA path
-        // Contoh: /about -> /about.html
-        // Contoh: /products/item -> /products/item.html
-        source: '/:path*',
-        destination: '/:path*.html',
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
